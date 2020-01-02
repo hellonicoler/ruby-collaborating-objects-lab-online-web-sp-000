06ce44de3f4c371c6391f0c93754c8c788114973
Nicole Rothenay <nthorn1211@gmail.com>
2020-01-02 22:42:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,37 +1,40 @@
 class Artist
-  attr_accessor :name
 
-  @@all = []
+    @@all = []
 
-  def initialize(name)
-    @name = name
-    @songs= []
-  end
+    attr_accessor :name
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-    song
-  end
+    def initialize(name)
+        @name = name
+        @@all << self
+        @songs = []
+    end
 
-  def songs
-    @songs
-  end
+    def self.all
+        @@all
+    end
 
-  def save
-    @@all << self
-    self
-  end
+    def add_song(song)
+        @songs << song
+    end
 
-  def self.all
-    @@all
-  end
+    def songs
+        Song.all.each do |song|
+            if song.artist == self
+                @songs << song
+            end
+        end
+        @songs
+    end
 
-  def self.find_or_create_by_name(name)
-    self.all.detect {|artist| artist.name == name} || Artist.new(name).save
-  end
+    def self.find_or_create_by_name(name)
+        self.all.detect {|artist| artist.name == name} || self.new(name)
+    end
 
-  def print_songs
-    self.songs.each {|song| puts song.name} 
-   end
+    def print_songs
+        @songs.each do |song|
+            puts song.name 
+        end    
+    end
+
 end
